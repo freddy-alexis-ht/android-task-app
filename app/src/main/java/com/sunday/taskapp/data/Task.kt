39c59517a9f054c1b4945b4b0d32c96753839bcc
Freddy2 <freddy.alexis.ht@gmail.com>
2023-09-13 17:15:33 -0500

    refactor: update classes to match the needs

--- app/src/main/java/com/sunday/taskapp/data/Task.kt
+++ app/src/main/java/com/sunday/taskapp/data/Task.kt
@@ -1,7 +1,17 @@
 package com.sunday.taskapp.data
 
 data class Task(
+    var index: Int = 0,
     var title: String = "Título",
     var description: String = "Ésta es una descripción ligeramente extensa para ver cómo se comporta la app con textos largos",
     var isDone: Boolean = false
 )
+
+fun getTasks() = (1..4).map { it ->
+    Task(
+        index = it-1,
+        title = "Título $it",
+        description = "Description $it: Ésta es una descripción ligeramente extensa para ver cómo se comporta la app con textos largos",
+        isDone = false
+    )
+}
--- app/src/main/java/com/sunday/taskapp/ui/task_list/TaskListEvent.kt
+++ app/src/main/java/com/sunday/taskapp/ui/task_list/TaskListEvent.kt
@@ -3,9 +3,9 @@ package com.sunday.taskapp.ui.task_list
 import com.sunday.taskapp.data.Task
 
 sealed class TaskListEvent {
-    object OnCheckBox: TaskListEvent()
+    data class OnCheckBox(val task: Task): TaskListEvent()
     data class OnTaskItem(val task: Task): TaskListEvent()
     object OnAddButton: TaskListEvent()
-    object OnDeleteIcon: TaskListEvent()
+    data class OnDeleteIcon(val task: Task): TaskListEvent()
     object OnUndoDeleteInSnackbar: TaskListEvent()
 }
